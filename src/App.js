595fca8ac02896f457d270e094eeb30fdb6a54e2
Your Name <you@example.com>
2025-03-03 18:28:30 +0530

    new test

--- src/App.js
+++ src/App.js
@@ -4,13 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Here, all the map data with various map boundries will shown.</h1>
-      <h1>new chnages has been applied.</h1>
-      <h1>new chnages has been applied.</h1>
-      <h1>new chnages has been applied. master a</h1>
-      <h1>new chnages has been applied. master b</h1>
-      <h1>new chnages has been applied. a</h1>
-      <h1>new chnages has been applied. b</h1>
+      <h1>✅ Here, all the map data with various map boundaries will be shown.</h1>
     </div>
   );
 }
